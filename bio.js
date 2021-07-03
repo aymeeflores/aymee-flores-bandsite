b7d6f6510764fb1889be0734f826f294fa74929d
aymeeflores <aymeeflores@users.noreply.github.com>
2021-07-03 16:23:04 -0400

    fixed comment functionality

--- bio.js
+++ bio.js
@@ -1,17 +1,65 @@
+let commmentArray = [
+  {
+    name: "Miles Acosta",
+    content:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    date: new Date(2020, 11, 20).toLocaleDateString(),
+  },
+  {
+    name: "Emilie Beach",
+    content:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    date: new Date(2021, 0, 09).toLocaleDateString(),
+  },
+  {
+    name: "Connor Walton",
+    content:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    date: new Date(2021, 1, 17).toLocaleDateString(),
+  },
+];
+
 let button = document.getElementById("commentButton");
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
   let name = document.getElementById("inputName");
   let comment = document.getElementById("inputComment");
-  let container = document.getElementById("comments_list");
   let date = new Date().toLocaleDateString();
+  let container = document.getElementById("comments_list");
 
   if (name.value === "" || comment.value === "") {
     name.classList.add("comments__input--error");
     comment.classList.add("comments__input--error");
     return;
   }
+  commmentArray.push({
+    name: name.value,
+    content: comment.value,
+    date: date,
+  });
+  container.innerHTML = "";
+  addAllComments();
+  if (name.classList.contains("comments__input--error")) {
+    name.classList.remove("comments__input--error");
+  }
+
+  if (comment.classList.contains("comments__input--error")) {
+    comment.classList.remove("comments__input--error");
+  }
+});
+
+function addAllComments() {
+  for (let i = 0; i < commmentArray.length; i++) {
+    displayComment(commmentArray[i]);
+  }
+}
+
+function displayComment(comment) {
+  let name = comment.name;
+  let content = comment.content;
+  let container = document.getElementById("comments_list");
+  let date = comment.date;
 
   let commentContainer = document.createElement("div");
   commentContainer.classList.add("comments__item");
@@ -33,29 +81,24 @@ button.addEventListener("click", function (e) {
 
   let commentsName = document.createElement("div");
   commentsName.classList.add("comments__item__name");
-  commentsName.innerText = name.value;
+  commentsName.innerText = name;
   commentsTitle.appendChild(commentsName);
 
   let commentsDate = document.createElement("div");
   commentsDate.classList.add("comments__item__date");
-  commentsDate.innerText = date.value;
+  commentsDate.innerText = date;
   commentsTitle.appendChild(commentsDate);
 
   rightSide.appendChild(commentsTitle);
 
   let commentContent = document.createElement("div");
   commentContent.classList.add("comments__item__content");
-  commentContent.innerText = comment.value;
+  commentContent.innerText = content;
   rightSide.appendChild(commentContent);
 
   commentContainer.appendChild(rightSide);
 
   container.prepend(commentContainer);
-  if (name.classList.contains("comments__input--error")) {
-    name.classList.remove("comments__input--error");
-  }
+}
 
-  if (comment.classList.contains("comments__input--error")) {
-    comment.classList.remove("comments__input--error");
-  }
-});
+addAllComments();
